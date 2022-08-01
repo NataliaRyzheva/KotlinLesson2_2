c7cbc2cc7663a181f6ccb7bd5c673ae86259e4bd
Natalya Ryzheva <tyta-ryzheva@yandex.ru>
2022-08-01 12:52:36 +0300

    git init

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val likes = 321
+    if((likes % 10) == 1 && (likes % 100) != 11 ) println("Понравилось $likes человеку") else println("Понравилось $likes людям")
+}
